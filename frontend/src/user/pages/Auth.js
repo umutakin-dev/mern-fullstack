6a1739920d446af9f7b5558da94f10a464e5016b
Umut Akin <umutakin@fastmail.com>
2021-05-14 16:53:31 +0300

    add an auth page and login form (mern-069)

---
+++ frontend/src/user/pages/Auth.js
@@ -0,0 +1,65 @@
+import React from "react";
+
+import Card from "../../shared/components/UIElements/Card";
+import Input from "../../shared/components/FormElements/Input";
+import Button from "../../shared/components/FormElements/Button";
+import { useForm } from "../../shared/hooks/form-hook";
+import {
+  VALIDATOR_EMAIL,
+  VALIDATOR_MINLENGTH,
+} from "../../shared/util/validators";
+
+import "./Auth.css";
+
+const Auth = () => {
+  const [formState, inputHandler] = useForm(
+    {
+      email: {
+        value: "",
+        isValid: false,
+      },
+      password: {
+        value: "",
+        isValid: false,
+      },
+    },
+    false
+  );
+
+  const authSubmitHandler = (event) => {
+    event.preventDefault();
+    console.log(formState.inputs);
+  };
+
+  return (
+    <Card className="authentication">
+      <h2>Login Required</h2>
+      <hr />
+      <form onSubmit={authSubmitHandler}>
+        <Input
+          id="email"
+          element="input"
+          type="email"
+          label="Email"
+          validators={[VALIDATOR_EMAIL()]}
+          errorText="Please enter a valid email address"
+          onInput={inputHandler}
+        />
+        <Input
+          id="password"
+          element="input"
+          type="password"
+          label="Password"
+          validators={[VALIDATOR_MINLENGTH(8)]}
+          errorText="Please enter a valid password, at least 8 characters."
+          onInput={inputHandler}
+        />
+        <Button type="submit" disabled={!formState.isValid}>
+          LOGIN
+        </Button>
+      </form>
+    </Card>
+  );
+};
+
+export default Auth;
