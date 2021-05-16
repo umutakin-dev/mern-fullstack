f2a941328341b860a6219a24523b40ef45524580
Umut Akin <umutakin@fastmail.com>
2021-05-16 20:10:27 +0300

    handle errors for unsupported routes (mern-095)

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const placesRoutes = require("./routes/places-routes");
+const HttpError = require("./models/http-error");
 
 const app = express();
 
@@ -9,6 +10,10 @@ app.use(bodyParser.json());
 //app.use(express.json());
 
 app.use("/api/places/", placesRoutes);
+
+app.use((req, res, next) => {
+  return next(new HttpError("Could not find this route.", 404));
+});
 
 app.use((error, req, res, next) => {
   if (res.headerSent) {
