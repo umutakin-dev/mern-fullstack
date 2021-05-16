af11125b16a4c176c58ece59c1ca823bf162014e
Umut Akin <umutakin@fastmail.com>
2021-05-16 19:24:22 +0300

    add our own error model (mern-092)

--- backend/routes/places-routes.js
+++ backend/routes/places-routes.js
@@ -1,22 +1,13 @@
 const express = require("express");
 
+const HttpError = require("../models/http-error");
+
 const router = express.Router();
 
 const DUMMY_PLACES = [
   {
     id: "p1",
     title: "empire state building",
-    description: "one of the most famous sky scrapers in the world",
-    location: {
-      lat: 40.7484474,
-      lng: -73.9871516,
-    },
-    address: "20 W 34th St, New York, NT 10001",
-    creator: "u1",
-  },
-  {
-    id: "p2",
-    title: "emp. state building",
     description: "one of the most famous sky scrapers in the world",
     location: {
       lat: 40.7484474,
@@ -34,9 +25,9 @@ router.get("/:pid", (req, res, next) => {
   });
 
   if (!place) {
-    const error = new Error("Could not find a place fot the provided id.");
-    error.code = 404;
-    next(error);
+    return next(
+      new HttpError("Could not find a place for the provided id.", 404)
+    );
   }
 
   res.json({ place });
@@ -49,11 +40,9 @@ router.get("/user/:uid", (req, res, next) => {
   });
 
   if (!place) {
-    const error = new Error(
-      "Could not find any place for the provided user id."
+    return next(
+      new Error("Could not find any place for the provided user id.", 404)
     );
-    error.code = 404;
-    return next(error);
   }
 
   res.json({ place });
