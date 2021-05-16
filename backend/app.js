c34176f54e6c20b38517181c6e4c847f06bb41c1
Umut Akin <umutakin@fastmail.com>
2021-05-16 21:45:01 +0300

    setup the user routes (signup, login, getusers) (mern-099)

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const placesRoutes = require("./routes/places-routes");
+const usersRoutes = require("./routes/users-routes");
 const HttpError = require("./models/http-error");
 
 const app = express();
@@ -9,7 +10,8 @@ const app = express();
 app.use(bodyParser.json());
 //app.use(express.json());
 
-app.use("/api/places/", placesRoutes);
+app.use("/api/places", placesRoutes);
+app.use("/api/users", usersRoutes);
 
 app.use((req, res, next) => {
   return next(new HttpError("Could not find this route.", 404));
---
+++ backend/controllers/users-controller.js
@@ -0,0 +1,59 @@
+const uuid = require("uuid");
+
+const HttpError = require("../models/http-error");
+
+const DUMMY_USERS = [
+  {
+    id: "u1",
+    name: "umut akin",
+    email: "asd@asd.com",
+    password: "test1234",
+  },
+];
+
+const getUsers = (req, res, next) => {
+  res.json({ users: DUMMY_USERS });
+};
+
+const signup = (req, res, next) => {
+  const { name, email, password } = req.body;
+
+  const hasUser = DUMMY_USERS.find((user) => user.email === email);
+  if (hasUser) {
+    return next(
+      new HttpError("could not create user, user already exists", 422)
+    );
+  }
+
+  const createdUser = {
+    id: uuid.v4(),
+    name,
+    email,
+    password,
+  };
+
+  DUMMY_USERS.push(createdUser);
+
+  res.status(201).json({ user: createdUser });
+};
+
+const login = (req, res, next) => {
+  const { email, password } = req.body;
+
+  const identifiedUser = DUMMY_USERS.find((user) => user.email === email);
+
+  if (!identifiedUser || identifiedUser.password !== password) {
+    return next(
+      new HttpError(
+        "could not identify user, credentials seem to be wrong.",
+        401
+      )
+    );
+  }
+
+  res.json({ message: "logged in" });
+};
+
+exports.getUsers = getUsers;
+exports.signup = signup;
+exports.login = login;
