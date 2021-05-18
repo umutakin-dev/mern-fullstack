629f82da1a247cfcbc44ce07dc192e2a1936d0c7
Umut Akin <umutakin@fastmail.com>
2021-05-18 12:03:18 +0300

    handle cors errors (mern-145)

--- backend/app.js
+++ backend/app.js
@@ -9,6 +9,16 @@ const app = express();
 
 app.use(express.json());
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
+  next();
+});
+
 app.use("/api/places", placesRoutes);
 app.use("/api/users", usersRoutes);
 
