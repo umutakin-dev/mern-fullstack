b720c9cf089e20c5048b1bcf3ae6e7def4f58735
Umut Akin <umutakin@fastmail.com>
2021-05-18 16:22:39 +0300

    create a custom http hook (mern-150)

---
+++ frontend/src/shared/hooks/http-hook.js
@@ -0,0 +1,45 @@
+import { useState, useCallback, useRef, useEffect } from "react";
+
+export const useHttpClient = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState();
+
+  const activeHttpRequests = useRef([]);
+
+  const sendRequest = useCallback(
+    async (url, method = "GET", body = null, header = {}) => {
+      setIsLoading(true);
+      const httpAbortController = new AbortController();
+      activeHttpRequests.current.push(httpAbortController);
+      try {
+        const response = await fetch(url, {
+          method,
+          body,
+          headers,
+          signal: httpAbortController.signal,
+        });
+        const responseData = await response.json();
+        if (!response.ok) {
+          throw new Error(responseData.message);
+        }
+        return responseData;
+      } catch (error) {
+        setError(error.message);
+      }
+      setIsLoading(false);
+    },
+    []
+  );
+
+  const clearError = () => {
+    setError(null);
+  };
+
+  useEffect(() => {
+    return () => {
+       activeHttpRequests.current.forEach(abortController => abortController.abort())
+    }
+  }, []);
+
+  return { isLoading, error, sendRequest, clearError };
+};
