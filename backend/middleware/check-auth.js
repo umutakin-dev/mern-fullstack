a85a8bc7bbf697476bbaa69bd24e33770beab3a5
Umut Akin <umutakin@fastmail.com>
2021-05-20 23:01:38 +0300

    setup backend route protection with auth middleware (mern-178)

---
+++ backend/middleware/check-auth.js
@@ -0,0 +1,17 @@
+const jwt = require("jsonwebtoken");
+const HttpError = require("../models/http-error");
+
+module.exports = (req, res, next) => {
+  try {
+    // Authorization: 'Bearer Token'
+    const token = req.headers.authorization.split(" ")[1];
+    if (!token) {
+      throw new Error();
+    }
+    const decodedToken = jwt.verify(token, "supersecret_dont_share");
+    req.userData = { userId: decodedToken.userId };
+    next();
+  } catch (error) {
+    return next(new HttpError("authentication failed!", 401));
+  }
+};
